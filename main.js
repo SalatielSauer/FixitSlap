02a4727eec95d3df504a1f319224c3bec23a0b33
SalatielSauer <tiel_chaves@hotmail.com>
2019-09-21 10:20:14 -0300

    another way to handle the frames

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 window.onload = function(){
 	frame = document.getElementById("frame");
+	frames = document.getElementById("frames");
 	pscore = document.getElementById("score");
 	score = 0;
 	curimg = 25;
 	oldx = 0;
+	loaded = 0;
+	preloadframes();
+};
+
+function preloadframes(){
+	for (i = 0; i <= 50; i++){
+		frames.innerHTML += '<img draggable="false" class="frame" id="frame-' + i + '" src="sprites/00' + i + '.jpg"/>';
+		if (i == 50){
+			loaded = 1;
+		};
+	};
 };
 
 function refreshimg(){
-
+	if (!loaded){return;};
+	oldimg = curimg;
 	if (curimg > 0){
 	    if (event.pageX < oldx) {
 	        curimg -= 1;
@@ -30,5 +43,7 @@ function refreshimg(){
 };
 
 function update(){
-	frame.src = "sprites/00" + curimg + ".png";
+	//frame.src = "sprites/00" + curimg + ".jpg";
+	document.getElementById("frame-" + oldimg).style.display = "none";
+	document.getElementById("frame-" + curimg).style.display = "unset";
 };
